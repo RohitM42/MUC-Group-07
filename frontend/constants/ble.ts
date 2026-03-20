b07d6b01ca54f5e3b996f6a3cda6aba378bc6da0
RohitM42 <rohitmamtora04@gmail.com>
2026-03-20 15:35:51 +0000

    ble bug fix

--- frontend/constants/ble.ts
+++ frontend/constants/ble.ts
@@ -1,4 +1,4 @@
 // ml + app enabled code/foot/foot.ino broadcasts with these identifiers
-export const DEVICE_NAME  = 'IMU_Foot';
+export const DEVICE_NAME  = 'IMU_Master1';
 export const SERVICE_UUID = '19B10040-E8F2-537E-4F6C-D104768A1214';
 export const CHAR_UUID    = '19B10041-E8F2-537E-4F6C-D104768A1214';
